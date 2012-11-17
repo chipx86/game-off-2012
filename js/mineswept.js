11076d343009b2f4ed97ce8a4c585d3a4d9d1162
Christian Hammond <chipx86@chipx86.com>
2012-11-17 00:24:54 -0800

    Make progress toward an interactive command processor and game engine.
    There's now a Game class that manages the state of the game, and a
    command interpreter that matches on regexes.
    
    Terminal support has been cleaned up and made much nicer. There's now a
    Buffer object that manages the screen buffer, which really just wraps
    the <pre>'s textContent attribute. Terminal can now manipulate this as
    it needs to.

--- js/mineswept.js
+++ js/mineswept.js
@@ -4,13 +4,94 @@
 var MS = {};
 
 MS.KeyCodes = {
+    BACKSPACE: 8,
     ENTER: 13
 };
 
 
+MS.CommandInterpreter = Backbone.Model.extend({
+    defaults: {
+        game: null
+    },
+
+    commands: [
+        ['^(go )?n(orth)?$', '_goNorth'],
+        ['^(go )?s(outh)?$', '_goSouth'],
+        ['^(go )?e(east)?$', '_goEast'],
+        ['^(go )?w(west)?$', '_goWest']
+    ],
+
+    initialize: function() {
+        var newCommands = [];
+
+        _.each(this.commands, function(item) {
+            var r = new RegExp(item[0]);
+            r.compile(r);
+            newCommands.push([r, item[1]]);
+        });
+
+        this.commands = newCommands;
+    },
+
+    handleCommand: function(line) {
+        var len = this.commands.length,
+            i;
+
+        for (i = 0; i < len; i++) {
+            var item = this.commands[i],
+                m = item[0].exec(line);
+
+            if (m) {
+                this[item[1]](m);
+
+                return true;
+            }
+        }
+
+        return false;
+    },
+
+    _goNorth: function() {
+        this.get('game').terminal.writeLine('Hello!');
+    },
+
+    _goSouth: function() {
+        console.log('south');
+    },
+
+    _goEast: function() {
+        console.log('east');
+    },
+
+    _goWest: function() {
+        console.log('west');
+    }
+});
+
+
 MS.Terminal = Backbone.Model.extend({
+    defaults: {
+        buffer: null
+    },
+
+    promptStr: '> ',
+
     initialize: function() {
         this._lineBuffer = '';
+        this._hasPrompt = false;
+    },
+
+    keyDown: function(e) {
+        switch (e.which) {
+            case MS.KeyCodes.BACKSPACE:
+                this._backspace();
+                break;
+
+            default:
+                return false;
+        }
+
+        return true;
     },
 
     keyPressed: function(e) {
@@ -20,55 +101,121 @@ MS.Terminal = Backbone.Model.extend({
             case MS.KeyCodes.ENTER:
                 this._processLine();
                 break;
+
             default:
-                this._addChar(String.fromCharCode(keyCode));
+                var c = String.fromCharCode(keyCode);
+                this._lineBuffer += c;
+                this._appendText(c);
                 break;
         }
+
+        return true;
+    },
+
+    write: function(line) {
+        var pos = (this._hasPrompt
+                   ? this._lineBuffer.length - this.promptStr.length
+                   : 0);
+
+        this._insertText(pos, line || '', true);
+    },
+
+    writeLine: function(line) {
+        this.write((line || '') + '\n');
     },
 
     clear: function() {
-        this.trigger('setText', '');
+        this._setText('');
+    },
+
+    showPrompt: function() {
+        this._hasPrompt = true;
+        this._appendText(this.promptStr);
     },
 
     _processLine: function() {
-        this.trigger('appendText', '\n');
+        var line = this._lineBuffer;
+        this._appendText('\n');
         this._lineBuffer = '';
-        this._showPrompt();
+        this.trigger('lineEntered', line);
+        this.showPrompt();
     },
 
-    _addChar: function(c) {
-        this._lineBuffer += c;
-        this.trigger('appendText', c);
+    _backspace: function() {
+        var lineLen = this._lineBuffer.length;
+
+        if (lineLen > 0) {
+            var buffer = this.get('buffer'),
+                bufferText = buffer.get();
+
+            this._lineBuffer = this._lineBuffer.slice(0, lineLen - 1);
+            buffer.set(bufferText.slice(0, bufferText.length - 1));
+        }
     },
 
-    _showPrompt: function() {
-        this.trigger('appendText', '> ');
+    _setText: function(s) {
+        this.get('buffer').set(s);
+    },
+
+    _insertText: function(pos, text, fromEnd) {
+        var buffer = this.get('buffer'),
+            bufferText = buffer.get(),
+            bufferLen = bufferText.length,
+            realPos = (fromEnd ? bufferLen - pos : pos);
+
+        if (realPos === bufferLen) {
+            buffer.append(text);
+        } else {
+            buffer.set([
+                bufferText.slice(0, realPos),
+                text,
+                bufferText.slice(realPos)
+            ].join(''));
+        }
+    },
+
+    _appendText: function(s) {
+        this.get('buffer').append(s);
     }
 });
+
+
+MS.Buffer = function(pre) {
+    this.append = function(s) {
+        pre.textContent += s;
+    };
+
+    this.set = function(s) {
+        pre.textContent = s;
+    };
+
+    this.get = function(s) {
+        return pre.textContent;
+    };
+
+    return this;
+};
 
 
 MS.TerminalView = Backbone.View.extend({
     el: $('#terminal'),
 
     initialize: function() {
-        $(document.body).keypress(_.bind(function(e) {
-            this.model.keyPressed(e);
-            return false;
+        this.model.set('buffer', new MS.Buffer(this.el));
+
+        $(document.body).keydown(_.bind(function(e) {
+            return !this.model.keyDown(e);
         }, this));
 
-        this.model.on('setText', function(s) {
-            this.el.textContent = s;
-        }, this);
-
-        this.model.on('appendText', function(s) {
-            this.el.textContent += s;
-        }, this);
+        $(document.body).keypress(_.bind(function(e) {
+            return !this.model.keyPressed(e);
+        }, this));
     },
 
     render: function() {
         this._updateSize();
 
-        $(window).resize(_.bind(this, _updateSize, this));
+        $(window).resize(_.bind(this._updateSize, this));
 
         return this;
     },
@@ -81,12 +228,66 @@ MS.TerminalView = Backbone.View.extend({
 });
 
 
+MS.Game = Backbone.Model.extend({
+    introText: [
+        "A late-night gelato run. Sounded like a good idea, only you didn't",
+        "make it that far.",
+        "",
+        "*THUD*",
+        "",
+        "You don't know how long you've been knocked out, but you awoke with ",
+        "a pretty bad headache.",
+        "",
+        "As your eyes begin to grow accustomed to the darkness, a shiver ",
+        "goes down your spine. Your worse fears have been realized:",
+        "",
+        "Somebody put you in an underground minefield."
+    ].join('\n'),
+
+    initialize: function() {
+        this.terminal = new MS.Terminal();
+        this.commandInterpreter = new MS.CommandInterpreter({
+            game: this
+        });
+
+        this.terminal.on('lineEntered', function(line) {
+            this.commandInterpreter.handleCommand(line);
+        }, this);
+    },
+
+    run: function() {
+        this.terminal.clear();
+        this.terminal.writeLine('= M I N E S W E P T =');
+        this.terminal.writeLine();
+        this.terminal.writeLine(this.introText);
+        this.terminal.writeLine();
+        this.terminal.showPrompt();
+    }
+});
+
+
+MS.GameView = Backbone.View.extend({
+    initialize: function() {
+        this.terminalView = new MS.TerminalView({
+            model: this.model.terminal
+        });
+    },
+
+    render: function() {
+        this.terminalView.render();
+
+        return this;
+    }
+});
+
+
 $(document).ready(function() {
-    var terminalView = new MS.TerminalView({
-        model: new MS.Terminal()
+    var gameView = new MS.GameView({
+        model: new MS.Game()
     });
 
-    terminalView.render();
+    gameView.render();
+    gameView.model.run();
 });
 
 
